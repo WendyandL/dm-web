4a86f264e0a16a425400357ec6729952c200deb9
wendyandl <1013105606@qq.com>
2022-11-10 22:22:02 +0800

    突发事件

--- src/components/event/EventShow.vue
+++ src/components/event/EventShow.vue
@@ -1,46 +1,49 @@
 <template>
   <div style="justify-content: center">
-    <el-dialog :visible.sync="pdfPreviewDialog" width="50%" center>
-      <div class="pdf">
-        <pdf :src="src" :page="currentPdf" @num-pages="pageCount=$event"
-             @page-loaded="currentPdf=$event" @loaded="loadPdfHandler"></pdf>
-        <p class="arrow" style="text-align: center">
-          <span @click="changePdfPage(0)" class="turn" :class="{grey: currentPdf==1}">上一页</span>
-          {{currentPdf}} / {{pageCount}}
-          <span @click="changePdfPage(1)" class="turn" :class="{grey: currentPdf==pageCount}">下一页</span>
-        </p>
-        </div>
+    <el-dialog :visible.sync="previewDialog" width="50%" center>
+      <div>{{detail}}</div>
     </el-dialog>
     <el-row style="margin: 18px 0 20px 18px">
       <el-breadcrumb separator-class="el-icon-arrow-right">
-        <el-breadcrumb-item>应急资源</el-breadcrumb-item>
-        <el-breadcrumb-item>应急预案管理</el-breadcrumb-item>
+        <el-breadcrumb-item>突发事件</el-breadcrumb-item>
+        <el-breadcrumb-item>突发事件管理</el-breadcrumb-item>
       </el-breadcrumb>
     </el-row>
     <el-card>
       <el-form>
         <el-row>
-          <el-col :span=10>
-            <el-form-item style="text-align: right">预案名称：</el-form-item>
+          <el-col :span=2>
+            <el-form-item>事件名称：</el-form-item>
           </el-col>
-          <el-col :span=6>
+          <el-col :span=10>
             <el-form-item>
               <el-input v-model="nameKeyword"></el-input>
             </el-form-item>
           </el-col>
+          <el-col :span=2>
+            <el-form-item>事件类型:</el-form-item>
+          </el-col>
+          <el-col :span=10>
+            <el-form-item>
+              <el-input v-model="typeKeyword"></el-input>
+            </el-form-item>
+          </el-col>
         </el-row>
-        <el-button size="small" type="primary" icon="el-icon-search" @click="loadInfo">搜索</el-button>
-        <el-button size="small" type="primary" icon="el-icon-delete" @click="resetKeyword">清空</el-button>
+      <el-button size="small" type="primary" icon="el-icon-search" @click="loadInfo">搜索</el-button>
+      <el-button size="small" type="primary" icon="el-icon-delete" @click="resetKeyword">清空</el-button>
       </el-form>
     </el-card>
     <div>
       <el-card>
-        <el-table :data="plans" style="width: 100%">
+        <el-table :data="events" style="width: 100%">
           <el-table-column prop="id" label="序号" v-if="false"></el-table-column>
-          <el-table-column prop="planName" label="预案名称" fit></el-table-column>
+          <el-table-column prop="eventName" label="事件名称" fit></el-table-column>
+          <el-table-column prop="eventTime" label="事件时间" fit></el-table-column>
+          <el-table-column prop="eventLocation" label="事件位置" fit></el-table-column>
+          <el-table-column prop="eventType" label="事件类型" fit></el-table-column>
           <el-table-column label="操作" width="120">
             <template slot-scope="scope">
-              <el-button @click="showPDF(scope.row)" type="text" size="small">预览</el-button>
+              <el-button @click="showDetail(scope.row)" type="text" size="small">查看详情</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -57,26 +60,20 @@
 </template>
 
 <script>
-import pdf from 'vue-pdf'
 
 export default {
-  name: 'PlanShow',
-  components: {
-    pdf
-  },
+  name: 'EventShow',
   data () {
     return {
-      plans: [],
+      events: [],
       currentPage: 1,
       totalCount: 0,
       pageSizes: [10, 20, 50],
       pageSize: 10,
       nameKeyword: '',
-      pdfUrl: '',
-      src: '',
-      pdfPreviewDialog: false,
-      currentPdf: 1,
-      pageCount: 0
+      typeKeyword: '',
+      detail: '',
+      previewDialog: false
     }
   },
   mounted () {
@@ -86,15 +83,16 @@ export default {
     loadInfo () {
       const _this = this
       this.$axios
-        .get('/resources/plan/search', {
+        .get('/event/search', {
           params: {
             nameKeyword: this.nameKeyword,
+            typeKeyword: this.typeKeyword,
             pageNum: this.currentPage,
             pageSize: this.pageSize
           }
         }).then(resp => {
           if (resp && resp.status === 200) {
-            _this.plans = resp.data.result.content
+            _this.events = resp.data.result.content
             _this.totalCount = resp.data.result.total
           }
         })
@@ -109,26 +107,14 @@ export default {
     },
     resetKeyword () {
       this.nameKeyword = ''
+      this.typeKeyword = ''
       this.currentPage = 1
       this.pageSize = 10
       this.loadInfo()
     },
-    showPDF (plan) {
-      this.pdfUrl = plan.planURL
-      this.src = pdf.createLoadingTask(this.pdfUrl)
-      this.currentPdf = 1
-      this.pdfPreviewDialog = true
-    },
-    changePdfPage (val) {
-      if (val === 0 && this.currentPdf > 1) {
-        this.currentPdf--
-      }
-      if (val === 1 && this.currentPdf < this.pageCount) {
-        this.currentPdf++
-      }
-    },
-    loadPdfHandler () {
-      this.currentPdf = 1 // 加载的时候先加载第一页
+    showDetail (event) {
+      this.detail = event.eventUrl
+      this.previewDialog = true
     }
   }
 }
